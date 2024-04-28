a395753a44e74df5e39932e5415a31e4a85845eb
Camilo <camilo.mardu@gmail.com>
2024-04-27 19:01:20 -0500

    metodos del servidor para avisar con vista y en consola

--- lectoTextParcial_II/Servidor/vista/WServidor.java
+++ lectoTextParcial_II/Servidor/vista/WServidor.java
@@ -2,6 +2,7 @@ package vista;
 
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
@@ -24,6 +25,22 @@ public class WServidor extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+	}
+	
+	/**
+	 * Muestra un aviso con el mensaje proporcionado.
+	 * @param mensaje El mensaje a mostrar.
+	 */
+	public void aviso(String mensaje) {
+		JOptionPane.showMessageDialog(null, mensaje);
+	}
+	
+	/**
+	 * Imprime un mensaje en la consola.
+	 * @param mensaje El mensaje a imprimir.
+	 */
+	public void enConsola(Object mensaje) {
+		System.out.println(mensaje);
 	}
 
 }
